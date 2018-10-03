8a6e121bacef184056ad2d3384d84dfe78f1c4a5
barkhyehyeon <barkhyehyeon@gmail.com>
2018-10-03 22:01:42 +0800

    Add IngredientParser

---
+++ src/main/java/seedu/address/logic/parser/IngredientParser.java
@@ -0,0 +1,82 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.parser.AddCommandParser;
+import seedu.address.logic.parser.DeleteCommandParser;
+import seedu.address.logic.parser.EditCommandParser;
+import seedu.address.logic.parser.FindCommandParser;
+import seedu.address.logic.parser.SelectCommandParser;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.ingredient.Ingredient;
+
+/**
+ * Parses user input related to ingredient.
+ */
+public class IngredientParser {
+
+    /**
+     * Used for initial separation of command word and args.
+     */
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+
+    /**
+     * Parses user input into command for execution.
+     *
+     *
+     * @param model
+     * @param userInput full user input string
+     * @return the command based on the user input
+     * @throws ParseException if the user input does not conform the expected format
+     */
+
+    public Command<Ingredient> parseCommand(Model model, String userInput) throws ParseException {
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments");
+        switch (commandWord) {
+
+            case AddCommand.COMMAND_WORD:
+                return new AddCommandParser().parseIngredient(model, arguments);
+
+            case EditCommand.COMMAND_WORD:
+                return new EditCommandParser().parseIngredient(model, arguments);
+
+            case SelectCommand.COMMAND_WORD:
+                return new SelectCommandParser().parseIngredient(model, arguments);
+
+            case DeleteCommand.COMMAND_WORD:
+                return new DeleteCommandParser().parseIngredient(model, arguments);
+
+            case ClearCommand.COMMAND_WORD:
+                return new ClearCommand<>(model);
+
+            case FindCommand.COMMAND_WORD:
+                return new FindCommandParser().parseIngredient(model, arguments);
+
+            case ListCommand.COMMAND_WORD:
+                return new ListCommand<>(model);
+
+            default:
+                throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+        }
+    }
+}
